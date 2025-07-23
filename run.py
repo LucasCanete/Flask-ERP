bc23b1737b849c36091246ce15deba7b24b4f7a8
LucasCanete <lucas10020081@hotmail.com>
2025-07-23 16:11:26 +0200

    Automatically open webbrowser with exe

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from erp_system import app
 
+import webbrowser
+import threading
+
+
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    threading.Timer(1.0, open_browser).start()
+    app.run(host="127.0.0.1", port=5000)
