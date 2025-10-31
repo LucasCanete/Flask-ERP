e616be1b508c572e1bfc7fe6ede36a3c0ecf3dbd
LucasCanete <lucas10020081@hotmail.com>
2025-10-31 16:51:26 +0100

    Added updated donwload page for reports

---
+++ erp_system/download/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, make_response, render_template
+from erp_system.utils.pdf import generate_pdf_for_date
+from erp_system.forms import DownloadForm
+from datetime import datetime
+
+download_bp = Blueprint('download_bp',__name__)
+
+@download_bp.route("/downloads", methods=['GET', 'POST'])
+def download_page():
+
+    form = DownloadForm()
+    if form.validate_on_submit():
+        print(f"Date: {form.chosen_date.data}")
+        dt = datetime.strptime(form.chosen_date.data, '%Y-%m-%d').date()
+        dt_str = dt.strftime("%d-%m-%Y")
+        pdf = generate_pdf_for_date(date=dt)
+
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = f'inline; filename=reporte_{dt_str}.pdf'
+        return response
+
+    return render_template("download_report.html",form=form )
